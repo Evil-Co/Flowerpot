5b7810168c54b5f78975c6e882a57b5bc26fec1d
Johannes Donath <johannesd@evil-co.com>
2014-02-28 04:07:54 -0800

    Created a simple base definition for configuration adapters.

---
+++ src/main/java/org/evilco/mc/flowerpot/configuration/IProxyConfiguration.java
@@ -0,0 +1,31 @@
+package org.evilco.mc.flowerpot.configuration;
+
+import org.evilco.mc.flowerpot.protocol.ServerListener;
+import org.evilco.mc.flowerpot.server.MinecraftServer;
+
+import java.util.List;
+
+/**
+ * @auhtor Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
+ */
+public interface IProxyConfiguration {
+
+	/**
+	 * Returns the default Minecraft server.
+	 * @return
+	 */
+	public MinecraftServer getDefaultServer ();
+
+	/**
+	 * Returns a list of configured listeners.
+	 * @return
+	 */
+	public List<ServerListener> getListenerList ();
+
+	/**
+	 * Returns the connection timeout.
+	 * @return
+	 */
+	public int getTimeout ();
+}
