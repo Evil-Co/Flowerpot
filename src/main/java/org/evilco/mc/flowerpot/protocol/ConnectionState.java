1569aef4e9644e3f70143b1f26c9efc9abd1db30
Johannes Donath <johannesd@evil-co.com>
2014-02-28 04:08:32 -0800

    Created a basic connection state representation.

---
+++ src/main/java/org/evilco/mc/flowerpot/protocol/ConnectionState.java
@@ -0,0 +1,72 @@
+package org.evilco.mc.flowerpot.protocol;
+
+import org.evilco.mc.flowerpot.protocol.packet.*;
+
+/**
+ * @auhtor Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
+ */
+public enum ConnectionState {
+	HANDSHAKE {
+		/**
+		 * Static Initialization
+		 */
+		{
+			INBOUND.registerPacket (0x00, HandshakePacket.class);
+		}
+	},
+	STATUS {
+		/**
+		 * Static Initialization
+		 */
+		{
+			INBOUND.registerPacket (0x00, StatusRequestPacket.class);
+
+			OUTBOUND.registerPacket (0x00, StatusResponsePacket.class);
+		}
+	};
+
+	/**
+	 * Stores the inbound protocol.
+	 */
+	protected final PacketRegistry INBOUND = new PacketRegistry (ConnectionDirection.INBOUND);
+
+	/**
+	 * Stores the outbound protocol.
+	 */
+	protected final PacketRegistry OUTBOUND = new PacketRegistry (ConnectionDirection.OUTBOUND);
+
+	/**
+	 * Returns the inbound registry.
+	 * @param isClient
+	 * @return
+	 */
+	public PacketRegistry getInboundRegistry (boolean isClient) {
+		return (isClient ? OUTBOUND : INBOUND);
+	}
+
+	/**
+	 * Returns the inbound registry.
+	 * @return
+	 */
+	public PacketRegistry getInboundRegistry () {
+		return getInboundRegistry (false);
+	}
+
+	/**
+	 * Stores the outbound registry.
+	 * @param isClient
+	 * @return
+	 */
+	public PacketRegistry getOutboundRegistry (boolean isClient) {
+		return (isClient ? INBOUND : OUTBOUND);
+	}
+
+	/**
+	 * Stores the outbound registry.
+	 * @return
+	 */
+	public PacketRegistry getOutboundRegistry () {
+		return getOutboundRegistry (false);
+	}
+}
