a9ea7b3f4b5f6d24e3b4c54beddb652927363cec
Johannes Donath <johannesd@evil-co.com>
2014-02-28 04:10:37 -0800

    Created basic packet exceptions.

---
+++ src/main/java/org/evilco/mc/flowerpot/protocol/packet/PacketException.java
@@ -0,0 +1,28 @@
+package org.evilco.mc.flowerpot.protocol.packet;
+
+/**
+ * @auhtor Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
+ */
+public abstract class PacketException extends Exception {
+
+	public PacketException () {
+		super ();
+	}
+
+	public PacketException (String message) {
+		super (message);
+	}
+
+	public PacketException (String message, Throwable cause) {
+		super (message, cause);
+	}
+
+	public PacketException (Throwable cause) {
+		super (cause);
+	}
+
+	protected PacketException (String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super (message, cause, enableSuppression, writableStackTrace);
+	}
+}
