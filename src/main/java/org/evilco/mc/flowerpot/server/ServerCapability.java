421beddcc5d5a1351d4d43d2fe247301b3808b76
Johannes Donath <johannesd@evil-co.com>
2014-03-02 06:41:49 -0800

    Added a basic definition for capabilities.

---
+++ src/main/java/org/evilco/mc/flowerpot/server/ServerCapability.java
@@ -0,0 +1,11 @@
+package org.evilco.mc.flowerpot.server;
+
+/**
+ * @auhtor Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
+ */
+public enum ServerCapability {
+	FALLBACK,
+	FORGE,
+	PROTOCOL
+}
