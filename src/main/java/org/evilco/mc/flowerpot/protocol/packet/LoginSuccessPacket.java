7ac19c53d117ea0c1b07d740555bd7217a8b729f
Johannes Donath <johannesd@evil-co.com>
2014-03-05 03:09:17 -0800

    Created a simple LoginSuccess packet.

---
+++ src/main/java/org/evilco/mc/flowerpot/protocol/packet/LoginSuccessPacket.java
@@ -0,0 +1,52 @@
+package org.evilco.mc.flowerpot.protocol.packet;
+
+import io.netty.buffer.ByteBuf;
+
+/**
+ * @auhtor Johannes Donath <johannesd@evil-co.com>
+ * @copyright Copyright (C) 2014 Evil-Co <http://www.evil-co.org>
+ */
+public class LoginSuccessPacket extends AbstractPacket {
+
+	/**
+	 * Stores the user's username.
+	 */
+	protected String username;
+
+	/**
+	 * Stores the user's UUID.
+	 */
+	protected String uuid;
+
+	/**
+	 * Constructs a new LoginSuccessPacket.
+	 * @param uuid
+	 * @param username
+	 */
+	public LoginSuccessPacket (String uuid, String username) {
+		super ();
+
+		this.uuid = uuid;
+		this.username = username;
+	}
+
+	/**
+	 * Constructs a new LoginSuccessPacket.
+	 * @param in
+	 */
+	public LoginSuccessPacket (ByteBuf in) {
+		super (in);
+
+		this.uuid = PacketUtility.readString (in);
+		this.username = PacketUtility.readString (in);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void writePacket (ByteBuf out) {
+		PacketUtility.writeString (this.uuid, out);
+		PacketUtility.writeString (this.username, out);
+	}
+}
