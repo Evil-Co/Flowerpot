672b54ac48ee8214dac716eb05cc81ee9b2574c8
Johannes Donath <johannesd@evil-co.com>
2014-03-05 04:15:35 -0800

    Updated client handler to support all login packets.

--- src/main/java/org/evilco/mc/flowerpot/protocol/ClientChannelHandler.java
+++ src/main/java/org/evilco/mc/flowerpot/protocol/ClientChannelHandler.java
@@ -1,13 +1,29 @@
 package org.evilco.mc.flowerpot.protocol;
 
+import com.google.common.base.Charsets;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.timeout.ReadTimeoutException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.evilco.mc.flowerpot.FlowerpotServer;
+import org.evilco.mc.flowerpot.authentication.AuthenticationCallback;
+import org.evilco.mc.flowerpot.authentication.IAuthenticationService;
 import org.evilco.mc.flowerpot.protocol.codec.MinecraftCodec;
-import org.evilco.mc.flowerpot.protocol.packet.AbstractPacket;
-import org.evilco.mc.flowerpot.protocol.packet.HandshakePacket;
+import org.evilco.mc.flowerpot.protocol.codec.MinecraftEncryptionCodec;
+import org.evilco.mc.flowerpot.protocol.packet.*;
+import org.evilco.mc.flowerpot.protocol.packet.data.StatusResponse;
+import org.evilco.mc.flowerpot.server.MinecraftServer;
+import org.evilco.mc.flowerpot.server.capability.Capability;
+import org.evilco.mc.flowerpot.server.capability.CapabilityKey;
+import org.evilco.mc.flowerpot.server.listener.ServerListener;
+
+import javax.crypto.SecretKey;
+import java.net.URLEncoder;
+import java.security.MessageDigest;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @auhtor Johannes Donath <johannesd@evil-co.com>
@@ -19,6 +35,26 @@ public class ClientChannelHandler extends ChannelHandlerAdapter {
 	 * Stores the internal logging instance.
 	 */
 	protected static final Logger logger = LogManager.getFormatterLogger (ClientChannelHandler.class);
+
+	/**
+	 * Stores the verify token.
+	 */
+	protected byte[] verifyToken = null;
+
+	/**
+	 * Stores the selected Minecraft server.
+	 */
+	protected MinecraftServer server = null;
+
+	/**
+	 * Stores the serverID.
+	 */
+	protected String serverID = null;
+
+	/**
+	 * Stores the requested player name.
+	 */
+	protected String username = null;
 
 	/**
 	 * {@inheritDoc}
@@ -48,7 +84,7 @@ public class ClientChannelHandler extends ChannelHandlerAdapter {
 	 * {@inheritDoc}
 	 */
 	@Override
-	public void channelRead (ChannelHandlerContext ctx, Object msg) throws Exception {
+	public void channelRead (final ChannelHandlerContext ctx, Object msg) throws Exception {
 		logger.debug ("Received message of type " + msg.getClass ().getName ());
 
 		// cast message
@@ -61,15 +97,152 @@ public class ClientChannelHandler extends ChannelHandlerAdapter {
 			// log
 			logger.debug ("Received handshake from %s: Requested server %s:%s (protocol version %s) with followup state %s.", ctx.channel ().remoteAddress ().toString (), handshakePacket.getServerAddress (), handshakePacket.getServerPort (), handshakePacket.getProtocolVersion (), handshakePacket.getNextState ());
 
+			// get server
+			Map<CapabilityKey<?>, Capability<?>> capabilityMap = new HashMap<> ();
+			capabilityMap.put (MinecraftServer.CAPABILITY_PROTOCOL, new Capability<Integer> (handshakePacket.getProtocolVersion ()));
+
+			MinecraftServer server = FlowerpotServer.getInstance ().getConfiguration ().getServerList ().matchServer (handshakePacket.getServerAddress (), handshakePacket.getServerPort (), capabilityMap);
+
+			if (server == null) server = FlowerpotServer.getInstance ().getConfiguration ().getServerList ().matchServer (handshakePacket.getServerAddress (), handshakePacket.getServerPort (), Arrays.asList (new CapabilityKey<?>[] {
+				MinecraftServer.CAPABILITY_FALLBACK
+			}));
+
+			// handle errors
+			if (server == null) {
+				// log
+				logger.fatal ("Could not find any matching server for request " + handshakePacket.getServerAddress () + ":" + handshakePacket.getServerPort () + " (protocol version " + handshakePacket.getProtocolVersion () + ").");
+
+				// close connection
+				ctx.channel ().close ();
+
+				// stop further execution
+				return;
+			}
+
+			// store server
+			this.server = server;
+
 			// switch state
 			switch (handshakePacket.getNextState ()) {
 				case STATUS:
 					MinecraftCodec.setProtocol (ctx, ConnectionState.STATUS);
 					break;
 				case LOGIN:
-					// TODO
+					MinecraftCodec.setProtocol (ctx, ConnectionState.LOGIN);
+
+					// verify protocol version
+					if (handshakePacket.getProtocolVersion () != ((Capability<Integer>) this.server.getCapability (MinecraftServer.CAPABILITY_PROTOCOL)).get ()) {
+						// log
+						logger.info ("Disconnecting %s: The client version does not match the server version (expected version %s but received %s).", ctx.channel ().remoteAddress (), ((Capability<Integer>) this.server.getCapability (MinecraftServer.CAPABILITY_PROTOCOL)).get (), handshakePacket.getProtocolVersion ());
+
+						// disconnect
+						String reason = (((Capability<Integer>) this.server.getCapability (MinecraftServer.CAPABILITY_PROTOCOL)).get () < handshakePacket.getProtocolVersion () ? "disconnect.outdatedServer" : "disconnect.outdatedClient");
+						ctx.writeAndFlush (new KickPacket (FlowerpotServer.getInstance ().getTranslation (reason)));
+						ctx.channel ().close ();
+					}
+
 					break;
 			}
+
+			return;
+		}
+
+		if (message instanceof StatusRequestPacket) {
+			// log
+			logger.debug ("Received status request from %s. Replying with correct server status (protocol version %s).", ctx.channel ().remoteAddress ().toString (), ((Capability<Integer>) this.server.getCapability (MinecraftServer.CAPABILITY_PROTOCOL)).get ());
+
+			// create data object
+			StatusResponse response = new StatusResponse ();
+
+			// add encoded favicon
+			// TODO: Add missing properties
+			response.favicon = FlowerpotServer.getInstance ().getEncodedServerIcon ();
+
+			// pass data to description
+			response.description.setText (String.format (response.description.getText (), FlowerpotServer.VERSION, FlowerpotServer.BUILD));
+
+			// send packet
+			ctx.writeAndFlush (new StatusResponsePacket (response));
+			return;
+		}
+
+		if (message instanceof PingPacket) {
+			PingPacket ping = ((PingPacket) message);
+
+			// log
+			logger.debug ("Received ping from %s. Replying with pong (%s).", ctx.channel ().remoteAddress ().toString (), ping.getTimestamp ());
+
+			// respond
+			ctx.writeAndFlush (ping);
+			return;
+		}
+
+		if (message instanceof LoginStartPacket) {
+			LoginStartPacket loginStartPacket = ((LoginStartPacket) message);
+
+			// store username
+			this.username = loginStartPacket.getUsername ();
+
+			// log
+			logger.trace ("User %s requested to be authenticated (account %s has been requested).", ctx.channel ().remoteAddress ().toString (), loginStartPacket.getUsername ());
+
+			// TODO: Allow servers to run in offline mode
+
+			// encryption
+			EncryptionRequestPacket encryptionRequestPacket = new EncryptionRequestPacket ();
+
+			// store token
+			this.verifyToken = encryptionRequestPacket.getVerifyToken ();
+			this.serverID = encryptionRequestPacket.getServerID ();
+
+			// send encryption request
+			ctx.writeAndFlush (encryptionRequestPacket);
+			return;
+		}
+
+		if (message instanceof EncryptionResponsePacket) {
+			EncryptionResponsePacket encryptionResponsePacket = ((EncryptionResponsePacket) message);
+
+			// get encryption key
+			SecretKey encryptionKey = encryptionResponsePacket.getSharedKey (FlowerpotServer.getInstance ().getKeyPair ().getPrivate (), this.verifyToken);
+
+			// log
+			logger.trace ("User %s replied with encryption key of size %s bytes. Switching to protocol encryption.", ctx.channel ().remoteAddress ().toString (), encryptionKey.getEncoded ().length);
+
+			// switch to encryption
+			ctx.channel ().pipeline ().addBefore (ServerListener.HANDLER_NAME_FRAME_CODEC, ServerListener.HANDLER_NAME_ENCRYPTION_CODEC, new MinecraftEncryptionCodec (encryptionKey));
+
+			// authenticate
+			FlowerpotServer.getInstance ().getAuthenticationService ().authenticate (this.username, this.serverID.getBytes (Charsets.UTF_8), encryptionKey.getEncoded (), FlowerpotServer.getInstance ().getKeyPair ().getPublic ().getEncoded (), ctx.channel ().eventLoop (), new AuthenticationCallback () {
+
+				/**
+				 * {@inheritDoc}
+				 */
+				@Override
+				public void error () {
+					ctx.channel ().writeAndFlush (new KickPacket (FlowerpotServer.getInstance ().getTranslation ("disconnect.authenticationServiceFailure")));
+				}
+
+				/**
+				 * {@inheritDoc}
+				 */
+				@Override
+				public void success (String uuid) {
+					if (uuid == null)
+						ctx.channel ().writeAndFlush (new KickPacket (FlowerpotServer.getInstance ().getTranslation ("disconnect.authenticationFailure")));
+					else {
+						// TODO: Kick already connected players with the same name
+
+						// send success packet
+						LoginSuccessPacket successPacket = new LoginSuccessPacket (uuid, username);
+						ctx.channel ().writeAndFlush (successPacket);
+
+						// update protocol
+						logger.info ("%s successfully authenticated with user ID %s.", username, uuid);
+						MinecraftCodec.setProtocol (ctx.channel (), ConnectionState.GAME);
+					}
+				}
+			});
 		}
 	}
 
@@ -88,6 +261,6 @@ public class ClientChannelHandler extends ChannelHandlerAdapter {
 		logger.debug ("An error occurred inside of a client connection.", cause);
 
 		// close connection
-		ctx.close ();
+		ctx.channel ().close ();
 	}
 }
